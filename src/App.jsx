3e4b467db7c6123e47fa7cb1ef01dddc4e8ac4f2
Nikhil-Arjun <nikhilarjun97@gmail.com>
2024-03-15 07:26:03 +0530

    React Setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./app.scss";
+const App = () => {
+  return (
+    <div>
+      <section>
+        <a href="">Hello</a>
+      </section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio-1</section>
+      <section>Portfolio-2</section>
+      <section>Portfolio-3</section>
+      <section>Portfolio-4</section>
+      <section>Portfolio-5</section>
+      <section>Portfolio-6</section>
+      <section>Contact</section>
+    </div>
+  );
+};
+
+export default App;
