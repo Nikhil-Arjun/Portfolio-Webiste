e0f013539b776412c76cc478839461c071ab8540
Nikhil-Arjun <nikhilarjun97@gmail.com>
2024-03-25 08:56:39 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -31,9 +31,7 @@ const App = () => {
         <Parallax type="portfolio" />
       </section>
       <Portfolio />
-      <section>
-        <About />
-      </section>
+
       <section id="Contact">
         <Contact />
         <Banner />
